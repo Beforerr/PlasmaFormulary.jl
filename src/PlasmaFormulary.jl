4ca02d0bea7a2879acbde6cc55bb0f65d2918bb1
Beforerr <zzj956959688@gmail.com>
2024-10-15 16:09:09 -0700

    feat: using constants from Unitful instead

--- src/PlasmaFormulary.jl
+++ src/PlasmaFormulary.jl
@@ -2,7 +2,9 @@ module PlasmaFormulary
 
 using Unitful
 using UnitfulEquivalences
-using Unitful: μ0, k
+using Unitful: μ0, ε0, c, q
+using Unitful: k, ħ
+using Unitful: me, mp
 
 @derived_dimension NumberDensity Unitful.𝐋^-3
 
@@ -12,16 +14,8 @@ temperature(eot) = uconvert(u"K", eot, Thermal())
 temperature(T:: Unitful.Temperature) = uconvert(u"K", T)
 
 # Physical Constants (SI)
-boltzmann_constant = Unitful.k
-elementry_charge = 1.60219e-19 * u"C"
-electron_mass = 9.10938e-31 * u"kg"
-proton_mass = 1.67262e-27 * u"kg"
 gravitational_constant = 6.67430e-11 * u"m^3 / s^2 / kg"
 planck_constant = 6.62607e-34 * u"J * s"
-reduced_planck_constant = 1.05457e-34 * u"J * s"
-light_speed = 2.99792e8 * u"m / s"
-free_space_permittivity = 8.85419e-12 * u"F / m"
-free_space_permeability = 1.25662e-6 *u"H / m"
 # Proton/electron mass ratio excluded
 # Electron charge/mass ratio excluded
 rydberg_constant = 1.09737e7 * u"m^-1"
@@ -49,26 +43,23 @@ molar_weight_of_air = 2.89647e-2 * u"kg / mol"
 # Calorie conversion omitted
 gravitational_acceleration = 9.80665 * u"m / s^2"
 
-# Common aliases
-k_B = Unitful.k
-
 # Fundamental plasma parameters
 # These formulas have been converted to use SI units from the original Gaussian cgs units
 # that are used in the 2023 edition of the formulary.
 function electron_gyrofrequency(magnetic_field::Unitful.BField)
-    upreferred(elementry_charge * magnetic_field / electron_mass)
+    upreferred(q * magnetic_field / me)
 end
 
-function ion_gyrofrequency(magnetic_field::Unitful.BField, charge_state::Integer, ion_mass::Unitful.Mass)
-    upreferred(charge_state * elementry_charge * magnetic_field / ion_mass)
+function ion_gyrofrequency(magnetic_field::Unitful.BField; charge_state::Integer = 1, ion_mass::Unitful.Mass = mp)
+    upreferred(charge_state * q * magnetic_field / ion_mass)
 end
 
 function electron_plasma_frequency(density::NumberDensity)
-    upreferred(sqrt(density * elementry_charge^2 / electron_mass / free_space_permittivity))
+    upreferred(sqrt(density * q^2 / me / ε0))
 end
 
 function ion_plasma_frequency(density::NumberDensity, charge_state::Integer, ion_mass::Unitful.Mass)
-    upreferred(sqrt(density * (charge_state * elementry_charge)^2 / ion_mass / free_space_permittivity))
+    upreferred(sqrt(density * (charge_state * q)^2 / ion_mass / ε0))
 end
 
 # electron and ion trapping rates excluded
@@ -76,11 +67,11 @@ end
 #electron and ion collision rates excluded
 
 function electron_debroglie_length(eot::EnergyOrTemp)
-    upreferred(sqrt(2 * pi * reduced_planck_constant^2 / electron_mass / energy(eot)))
+    upreferred(sqrt(2 * pi * ħ^2 / me / energy(eot)))
 end
 
 function classical_minimum_approach_distance(eot::EnergyOrTemp)
-    upreferred(elementry_charge^2 / energy(eot) / (4 * pi * free_space_permittivity))
+    upreferred(q^2 / energy(eot) / (4 * pi * ε0))
 end
 
 # TODO: electron_gyryradius
@@ -88,23 +79,23 @@ end
 # TODO: ion_gyroradius
 
 function electron_inertial_length(density::NumberDensity)
-    upreferred(light_speed / electron_plasma_frequency(density))
+    upreferred(c / electron_plasma_frequency(density))
 end
 
 function ion_inertial_length(density::NumberDensity, charge_state::Integer, ion_mass::Unitful.Mass)
-    upreferred(light_speed / ion_plasma_frequency(density, charge_state, ion_mass))
+    upreferred(c / ion_plasma_frequency(density, charge_state, ion_mass))
 end
 
 function debye_length(density::NumberDensity, eot::EnergyOrTemp)
-    upreferred(sqrt(free_space_permittivity * energy(eot) / density / elementry_charge^2))
+    upreferred(sqrt(ε0 * energy(eot) / density / q^2))
 end
 
 function electron_thermal_velocity(eot::EnergyOrTemp)
-    upreferred(sqrt(boltzmann_constant * temperature(eot) / electron_mass))
+    upreferred(sqrt(k * temperature(eot) / me))
 end
 
 function ion_thermal_velocity(eot::EnergyOrTemp, ion_mass::Unitful.Mass)
-    upreferred(sqrt(boltzmann_constant * temperature(eot) / ion_mass))
+    upreferred(sqrt(k * temperature(eot) / ion_mass))
 end
 
 # TODO: ion_sound_velocity
@@ -123,7 +114,7 @@ Calculate the thermal pressure for a Maxwellian distribution.
 - `n`: The particle number density.
 """
 function thermal_pressure(T::EnergyOrTemp, n::NumberDensity)
-    return n * k_B * temperature(T) |> upreferred
+    return n * k * temperature(T) |> upreferred
 end
 
 p_th = thermal_pressure
