6f9b32139f1e74ff31da82f7f7fccbd943387ee2
Beforerr <zzj956959688@gmail.com>
2024-11-13 11:49:50 -0800

    feat: Refactor gyroradius function and add additional length calculations

--- src/lengths.jl
+++ src/lengths.jl
@@ -1,4 +1,5 @@
 export gyroradius, rc_
+export debye_length
 
 """
 Calculate the radius of circular motion for a charged particle in a uniform magnetic field
@@ -11,16 +12,46 @@ julia> gyroradius(0.2u"T", 1e6u"K")
 0.006682528174870854 m
 ```
 """
-function gyroradius(B, Vperp::Velocity; mass_numb=1, Z=1)
-    mass = mass_numb * u
-    return upreferred(mass * Vperp / (Z * q * B))
+function gyroradius(B, mass, q, Vperp::Velocity)
+    return upreferred(mass * Vperp / (q * B))
 end
 
-function gyroradius(B, T::EnergyOrTemp; mass_numb=1, Z=1)
+function gyroradius(B, Vperp::Velocity; mass_numb = 1, Z = 1)
+    return gyroradius(B, mass_numb * Unitful.u, Z * Unitful.q, Vperp)
+end
+
+function gyroradius(B, T::EnergyOrTemp; mass_numb = 1, Z = 1)
     Vperp = thermal_speed(T, mass_numb)
     return gyroradius(B, Vperp; mass_numb, Z)
 end
 
-gyroradius(val::Union{Velocity, EnergyOrTemp}, B::BField; kw...) = gyroradius(B, val; kw...)
+gyroradius(val::Union{Velocity,EnergyOrTemp}, B::BField; kw...) = gyroradius(B, val; kw...)
+
+electron_gyroradius(B, Vperp::Velocity) = gyroradius(B, me, Unitful.q, Vperp)
+electron_gyroradius(B, T::EnergyOrTemp) = electron_gyroradius(B, thermal_speed(T, me))
 
 const rc_ = gyroradius
+
+# electron and ion trapping rates excluded
+
+#electron and ion collision rates excluded
+
+function electron_debroglie_length(eot::EnergyOrTemp)
+    upreferred(sqrt(2 * pi * ħ^2 / me / energy(eot)))
+end
+
+function classical_minimum_approach_distance(eot::EnergyOrTemp)
+    upreferred(q^2 / energy(eot) / (4 * pi * ε0))
+end
+
+function electron_inertial_length(n::NumberDensity)
+    upreferred(c / electron_plasma_frequency(n))
+end
+
+function ion_inertial_length(n::NumberDensity, Z, mass::Unitful.Mass)
+    upreferred(c / ion_plasma_frequency(n, Z, mass))
+end
+
+function debye_length(density::NumberDensity, eot::EnergyOrTemp)
+    upreferred(sqrt(ε0 * energy(eot) / density / q^2))
+end
