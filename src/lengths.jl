97b7eef53b218923531fabd10de6ce0e6dc6e2a2
Beforerr <zzj956959688@gmail.com>
2024-11-15 14:19:10 -0800

    doc: update

--- src/lengths.jl
+++ src/lengths.jl
@@ -56,7 +56,9 @@ end
 """
     inertial_length(n::NumberDensity, p::ParticleLike; kw...)
 
-The inertial length is the characteristic length scale for a particle to be accelerated in a plasma. 
+Calculate a charged particle's inertial length. 
+
+The inertial length is the characteristic length scale for a particle to be accelerated in a plasma.
 The Hall effect becomes important on length scales shorter than the ion inertial length.
 
 References: [PlasmaPy API Documentation](https://docs.plasmapy.org/en/latest/api/plasmapy.formulary.lengths.inertial_length.html)
