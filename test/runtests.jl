b28cb08a5a8bb8a3a9a28635318110740e977b39
Beforerr <zzj956959688@gmail.com>
2025-05-04 22:53:48 -0700

    CI: fix setup

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,19 +1,19 @@
 using TestItems, TestItemRunner
-using Unitful
-using LinearAlgebra
-using PythonCall
 using CondaPkg
-
 CondaPkg.add(["astropy", "plasmapy"])
 
-include("test_utils.jl")
-
-units = pyimport("astropy.units")
-formulary = pyimport("plasmapy.formulary")
+@testsnippet Py begin
+    using PythonCall
+    include("test_utils.jl")
+    units = pyimport("astropy.units")
+    formulary = pyimport("plasmapy.formulary")
+end
 
 @run_package_tests
 
 @testitem "Misc" begin
+    using Unitful
+    using LinearAlgebra
     @test gyroradius(0.2u"T", Unitful.mp, Unitful.q, 1e6u"K") ≈ 0.0067u"m" rtol = 0.01
     @test_broken electron_gyroradius(1e6u"K", 0.2u"T", Unitful.q, Unitful.mp) ≈ 0.0067u"m" rtol =
         0.01
@@ -31,7 +31,9 @@ formulary = pyimport("plasmapy.formulary")
     @test alfven_velocity.(B, rho) ≈ [-43185.625u"m/s", 0.0u"m/s", 0.0u"m/s"]
 end
 
-@testitem "dimensionless.jl" begin
+@testitem "dimensionless.jl" setup = [Py] begin
+    using Unitful
+
     T = 2.0u"eV"
     n = 1e19u"m^-3"
     B = 0.1u"T"
