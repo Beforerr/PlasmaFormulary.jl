c19158cf205b257159560e0b8f26a43741c48735
Beforerr <zzj956959688@gmail.com>
2025-05-05 00:55:48 -0700

    feat: providing aliases

--- src/alias.jl
+++ src/alias.jl
@@ -1,9 +1,25 @@
-module Alias
+"""
+This module provides aliases of the most common plasma functionality for user convenience. 
+
+Aliases are denoted with a trailing underscore (e.g., `alias_`).
+"""
+module Aliases
 using ..PlasmaFormularyZ
 
+# Ref: https://docs.plasmapy.org/en/stable/formulary/drifts.html
 const vd_ = diamagnetic_drift
 const veb_ = ExB_drift
 const vfd_ = force_drift
 export vd_, veb_, vfd_
 
+# Ref: https://docs.plasmapy.org/en/stable/formulary/lengths.html
+const rc_ = gyroradius
+const rhoc_ = gyroradius
+const cwp_ = inertial_length
+export rc_, rhoc_, cwp_
+
+# https://docs.plasmapy.org/en/stable/formulary/speeds.html
+const va_ = Alfven_speed
+const vth_ = thermal_velocity
+export va_, vth_
 end
